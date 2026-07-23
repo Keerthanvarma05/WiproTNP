26e1465013c3a288043ecd447ff07fcb66635ab1
Keerthan Varma <varmakeerthan9@gmail.com>
2026-07-23 14:05:27 +0530

    Day 1 Java practice

---
+++ Oops_Inheritence/Inheritence/AnimalDemo.java
@@ -0,0 +1,22 @@
+package Inheritence;
+
+public class AnimalDemo {
+
+    public static void main(String[] args) {
+
+        Animal a = new Animal();
+
+        System.out.println("Animal Class:");
+        a.eat();
+        a.sleep();
+
+        System.out.println();
+
+        Bird b = new Bird();
+
+        System.out.println("Bird Class:");
+        b.eat();
+        b.sleep();
+        b.fly();
+    }
+}
---
+++ Oops_Inheritence/Overriding_Polymorphism/Apple.java
@@ -0,0 +1,13 @@
+package Overriding_Polymorphism;
+
+public class Apple extends Fruit {
+
+    public Apple() {
+        super("Apple", "Sweet", "Medium");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Apple tastes Sweet");
+    }
+}
